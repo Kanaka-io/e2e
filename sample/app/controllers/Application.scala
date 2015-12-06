5f1824d57309e86aada2c70aa4f9471fd86a73ee
Valentin Kasas <valentin.kasas@gmail.com>
2015-12-06 01:06:55 +0100

    Fix sample project injection

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -1,14 +1,16 @@
 package controllers
 
+import javax.inject.Inject
+
 import play.api._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 import io.kanaka.e2e.play.Messages
 
-class Application(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class Application @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index = Action {
-    Ok(views.html.index(Messages("user.greet")))
+    Ok(views.html.index(Messages("user.greet", "Worldclean")))
   }
 
   def missing = Action {
